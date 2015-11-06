fe5d25300141ed09864216bededb64e76e39a072
prashanthavvari <apprashanth55@gmail.com>
2015-11-06 12:59:58 +0530

    Submit Commit

--- src/sortedArrayInsertNumber.cpp
+++ src/sortedArrayInsertNumber.cpp
@@ -16,5 +16,28 @@ NOTES: Use realloc to allocate memory.
 
 int * sortedArrayInsertNumber(int *Arr, int len, int num)
 {
-	return NULL;
+	int i,pos=0,c=0;
+	if (len < 0)
+		return NULL;
+	if (Arr == NULL)
+		return NULL;
+	for (i = 0; i < len; i++)
+	{
+		if (Arr[i]>num)
+		{
+			pos = i;
+			c = 1;
+			break;
+		}
+	}
+	len = len + 1;
+	if (c == 0)
+		Arr[i] = num;
+	else
+	{
+		for (i = len-1; i > pos; i--)
+			Arr[i] = Arr[i - 1];
+		Arr[pos] = num;
+	}
+	return Arr;
 }
